94225d760fe6875ae7bbe6a0ee62850720eb95f1
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2021-12-31 01:14:09 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,27 @@
+//Bubble Sort
+/*
+Input:
+    5
+    arr[]={5, 1, 4, 2, 8}
+Output:
+    1 2 4 5 8
+Explanation:
+    First Pass: 
+        ( 5 1 4 2 8 ) –> ( 1 5 4 2 8 )
+        ( 1 5 4 2 8 ) –> ( 1 4 5 2 8 )
+        ( 1 4 5 2 8 ) –> ( 1 4 2 5 8 ) 
+        ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+    Second Pass: 
+        ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ) 
+        ( 1 4 2 5 8 ) –> ( 1 2 4 5 8 )
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+    Third Pass: 
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+        ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+*/
 import java.util.*;
 public class BubbleSort {
     public static void main(String[] args){
